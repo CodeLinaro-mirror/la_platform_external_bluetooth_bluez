d016b69d766b3a49c4278b129226d3b835bdae52
Prabhakaran Mc <prabhakaranmc@codeaurora.org>
2011-09-21 16:29:02 +0530

    Bluetooth: If remote device's SEIDs are busy, disconnect avdtp channel
    During a2dp connection establishment, if remote device reports that
    their stream end points are in use with other devices in discover
    response then disconnect avdtp channel, this helps in updating UI
    about a2dp connection state.
    
    CRs-fixed: 308016
    (cherry picked from commit 4d08278dda2eb8e0907aa9f1a3c8696460e6b1a6)
    
    Change-Id: I3d28d46bcbdda4952b32316f15c5300cd2f81afb

--- audio/avdtp.c
+++ audio/avdtp.c
@@ -2751,8 +2751,14 @@ static gboolean avdtp_discover_resp(struct avdtp *session,
 
 		sep = find_remote_sep(session->seps, resp->seps[i].seid);
 		if (!sep) {
-			if (resp->seps[i].inuse && !stream)
-				continue;
+			if (resp->seps[i].inuse && !stream) {
+				if ((i + 1) == sep_count) {
+					finalize_discovery(session, -EINPROGRESS);
+					return TRUE;
+				}
+				else
+					continue;
+			}
 			sep = g_new0(struct avdtp_remote_sep, 1);
 			session->seps = g_slist_append(session->seps, sep);
 		}
