835daaeecc1ac37cdf5f0b0749af02f5b88752f0
Rohit Singh <singhr@codeaurora.org>
2012-09-04 20:59:10 +0530

    Bluetooth: Block a2dp stream during start
    During A2DP start it takes 1 sec for initial setup
    Block out stream for number of frames/sampling freq
    till a2dp state move to stream started.
    
    CRs-Fixed: 397602
    
    (cherry picked from commit 36fe83c5033ae118f5287ec57b104cbc3b96761e)
    
    Conflicts:
    
    	audio/android_audio_hw.c
    
    Change-Id: I8673a93dd1d63a70a9364bbe21503b6b132a6519

--- audio/android_audio_hw.c
+++ audio/android_audio_hw.c
@@ -522,6 +522,10 @@ static void *_out_buf_thread_func(void *context)
                 if (ret < 0) {
                     if (ret != -EINPROGRESS)   //not a Signaling start
                         LOGE("%s: a2dp_write failed (%d)\n", __func__, ret);
+                    if ( ret == -EINPROGRESS) {
+                        LOGE( "Sleep for frames/sampling freq %d",frames);
+                        usleep(((frames * 1000) / out->sample_rate) * 1000 ); //frames /sampling
+                    }
                     /* skip pending frames in case of write error */
                     _out_inc_rd_idx_locked(out, frames);
                     break;
