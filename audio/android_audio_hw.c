venky99/android_external_bluetooth_bluez

Bluetooth: Block a2dp stream during start
During A2DP start it takes 1 sec for initial setup
Block out stream for number of frames/sampling freq
till a2dp state move to stream started.

CRs-Fixed: 397602

Change-Id: I415c8405cb9956e0ba675199d84779fda25baa83

audio/android_audio_hw.c
@@ -526,6 +526,10 @@ static void *_out_buf_thread_func(void *context)
                 if (ret < 0) {
                     if (ret != -EINPROGRESS)   //not a Signaling start
                         ALOGE("%s: a2dp_write failed (%d)\n", __func__, ret);
+                    if ( ret == -EINPROGRESS) {
+                        ALOGE( "Sleep for frames/sampling freq %d",frames);
+                        usleep(((frames * 1000) / out->sample_rate) * 1000 ); //frames /sampling
+                    }
                     /* skip pending frames in case of write error */
                     _out_inc_rd_idx_locked(out, frames);
                     break;

CodeLinaro-mirror/la_platform_external_bluetooth_bluez

(cherry picked from commit 36fe83c5033ae118f5287ec57b104cbc3b96761e)
Conflicts:
	audio/android_audio_hw.c
Change-Id: Iaa086ce3a02bd6820c7673eeaea8930e2c24e846

audio/android_audio_hw.c
@@ -522,6 +522,10 @@ static void *_out_buf_thread_func(void *context)
                 if (ret < 0) {
                     if (ret != -EINPROGRESS)   //not a Signaling start
                         LOGE("%s: a2dp_write failed (%d)\n", __func__, ret);
+                    if ( ret == -EINPROGRESS) {
+                        LOGE( "Sleep for frames/sampling freq %d",frames);
+                        usleep(((frames * 1000) / out->sample_rate) * 1000 ); //frames /sampling
+                    }
                     /* skip pending frames in case of write error */
                     _out_inc_rd_idx_locked(out, frames);
                     break;