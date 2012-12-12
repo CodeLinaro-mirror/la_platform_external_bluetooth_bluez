303780890b7cb2c12e84e5e0218a355c9dfa4f2f
Rohit Singh <singhr@codeaurora.org>
2012-12-12 19:02:08 +0530

    Bluetooth: Pause Music Player on recieving Suspend
    Few headsets does not send AVRCP Pause but only
    send a2dp suspend.In that case Music player is
    not paused. Change is made to pause Media player
    on recieving a2dp suspend.
    
    CRs-Fixed: 436824
    
    Change-Id: I02ec5d5877d3df8f00632ac6736cabe02da9de66

--- audio/a2dp.c
+++ audio/a2dp.c
@@ -1048,6 +1048,13 @@ static gboolean suspend_ind(struct avdtp *session, struct avdtp_local_sep *sep,
 		a2dp_sep->session = NULL;
 	}
 
+	dev = a2dp_get_dev(session);
+	DBG("dev value is %p: ", dev);
+	if (dev) {
+		control_suspend(dev);
+		a2dp_sep->remote_suspend = TRUE;
+	}
+
 	return TRUE;
 }
 
