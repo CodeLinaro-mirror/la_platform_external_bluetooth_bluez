f2b367909dc9c1983c335c0012270bcb8420491e
Ayan Ghosh <abghosh@codeaurora.org>
2013-03-29 11:05:22 +0530

    Revert: Reject incoming a2dp connection from a second paired device
    Checking the session list in avdtp_confirm_cb to know if the device is
    already connected and accordingly taking decision to connect/reject.
    
    CRs-Fixed: 472079
    
    Change-Id: I2275a9e3907b83e7867e1f3a3f90f3295e5e11aa

--- audio/avdtp.c
+++ audio/avdtp.c
@@ -2561,7 +2561,6 @@ static void avdtp_confirm_cb(GIOChannel *chan, gpointer data)
 	bdaddr_t src, dst;
 	int perr;
 	GError *err = NULL;
-	struct avdtp_server *server;
 
 
 	bt_io_get(chan, BT_IO_L2CAP, &err,
@@ -2576,17 +2575,6 @@ static void avdtp_confirm_cb(GIOChannel *chan, gpointer data)
 	}
 
 	DBG("AVDTP: incoming connect from %s", address);
-
-	//check if there already exists a a2dp connection, discard the second device connection
-	server = find_server(servers, &src);
-	if (!server) {
-		goto drop;
-	}
-
-	if (server->sessions) {
-		DBG("AVDTP: reject incoming connect from 2nd device %s", address);
-		goto drop;
-	}
 
 	session = avdtp_get_internal(&src, &dst);
 	if (!session)
