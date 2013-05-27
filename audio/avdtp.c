447881d69dd77ef52830600fa9cdc6b1f4e7aed8
Ayan Ghosh <abghosh@codeaurora.org>
2013-05-27 19:16:33 +0530

    Fix crash in Bluez
    Check for validity of local sep pointer before dereferencing
    the same to avoid crash in Bluez.
    
    CRs-Fixed: 493769
    
    Change-Id: Ia0ac09dc413fa06cb0b52b8cd50a7e4d3b453f10

--- audio/avdtp.c
+++ audio/avdtp.c
@@ -1328,7 +1328,7 @@ struct avdtp_remote_sep *avdtp_find_remote_sep(struct avdtp *session,
 {
 	GSList *l;
 
-	if (lsep->info.inuse)
+	if ((lsep == NULL) || (lsep->info.inuse))
 		return NULL;
 
 	for (l = session->seps; l != NULL; l = g_slist_next(l)) {
